53d3e9cce75254edc8f7f32dc5428d8d52ac6c66
intrnl <intrnl28+git@gmail.com>
2020-10-12 11:58:28 +0700

    feat: add step to range

--- lib/creators.ts
+++ lib/creators.ts
@@ -3,6 +3,6 @@
  * @param start Number to start from
  * @param end Number to end with
  */
-export function* range (start: number, end: number) {
-	for (let i = start; i <= end; i++) yield i;
+export function* range (start: number, end: number, step: number = 1) {
+	for (let i = start; i <= end; i += step) yield i;
 }
