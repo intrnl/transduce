9ae2b57e07c983b2ccf30d4e8bdd6ad202fbee42
intrnl <intrnl28+git@gmail.com>
2020-10-12 12:00:31 +0700

    fix: proper pipe return type

--- lib/core.ts
+++ lib/core.ts
@@ -2,6 +2,6 @@
  * Pipes a value from one function to the next
  * @param fns Array of functions
  */
-export function pipe (fns: ((value: any) => any)[]): unknown {
+export function pipe (fns: ((value: any) => any)[]): (value: any) => unknown {
 	return fns.reduce((prev, next) => (value) => next(prev(value)));
 }
